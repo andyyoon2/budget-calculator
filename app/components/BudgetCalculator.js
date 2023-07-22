0cd78812a8ee02f6692daa22f9cf075750af2f1d
Andy Yoon <insoo.yoon@proton.me>
2023-07-21 18:51:51 -0700

    Split server and client components

---
+++ app/components/BudgetCalculator.js
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+import DateSelector, { months } from './DateSelector';
+import MonarchButton from './MonarchButton';
+import Spending from './Spending';
+
+export default function BudgetCalculator() {
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth());
+
+  return (
+    <>
+      <DateSelector
+        year={year}
+        setYear={setYear}
+        month={month}
+        setMonth={setMonth}
+      />
+      <MonarchButton
+        year={year}
+        month={month}
+      />
+      <h2>Spending report for {months[month]} {year}</h2>
+      <Spending />
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,34 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { useState } from 'react';
-import DateSelector, { months } from './components/DateSelector';
-import MonarchButton from './components/MonarchButton';
-import Spending from './components/Spending';
+import BudgetCalculator from './components/BudgetCalculator';
 
 export default function Home() {
-  const [year, setYear] = useState(new Date().getFullYear());
-  const [month, setMonth] = useState(new Date().getMonth());
-
   return (
     <div>
       <main class={styles.main}>
         <h1>Hello budget calculator</h1>
-        <div>
-          <DateSelector
-            year={year}
-            setYear={setYear}
-            month={month}
-            setMonth={setMonth}
-          />
-          <MonarchButton
-            year={year}
-            month={month}
-          />
-        </div>
-        <div>
-          <h2>Spending report for {months[month]} {year}</h2>
-          <Spending />
-        </div>
+        <BudgetCalculator />
       </main>
     </div>
   );
