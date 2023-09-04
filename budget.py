1c30e62394166551eee0a21c2fcb5e9c02ac464b
Andy Yoon <insoo.yoon@proton.me>
2023-09-04 10:32:38 -0700

    Adjust script with simpler tagging system

--- budget.py
+++ budget.py
@@ -1,32 +1,45 @@
+def round_num(n):
+    return round(n, 2)
+
 print("Total spending in shared categories:")
-total_spending = float(input())
-
-print("인수 only:")
-insoo_only = float(input())
-
-print("재민 only:")
-jaemin_only = float(input())
-
-total_shared = total_spending - insoo_only - jaemin_only
-print(f"total_shared: {total_shared}")
+total_spending = round_num(float(input()))
 
 # TODO: Option to adjust from JK or to JK (usually will be from JK)
 print("Refunds to different accounts:")
-refunds = float(input())
+refunds = round_num(float(input()))
 
 print("Total from 재민 accounts:")
-total_jaemin_accounts = float(input())
+total_jaemin_accounts = round_num(float(input()))
 
 print("Total from 인수 accounts:")
-total_insoo_accounts = float(input())
+total_insoo_accounts = round_num(float(input()))
 
-jaemin_withdraw = total_jaemin_accounts - jaemin_only + refunds
-insoo_withdraw = total_insoo_accounts - insoo_only - refunds
+print("Total from SoFi accounts:")
+total_sofi_accounts = round_num(float(input()))
 
-print(f"Total for 재민 to withdraw from SoFi: {jaemin_withdraw}")
-print(f"Total for 인수 to withdraw from SoFi: {insoo_withdraw}")
+jaemin_paid = round_num(total_jaemin_accounts + refunds)
+insoo_paid = round_num(total_insoo_accounts - refunds)
+half_split = round_num((total_spending - total_sofi_accounts - refunds) / 2)
 
-difference = total_shared - jaemin_withdraw - insoo_withdraw
+print(f"Total shared that 재민 paid: ${jaemin_paid}")
+print(f"Total shared that 인수 paid: ${insoo_paid}")
+print(f"50/50 split is ${half_split}")
+
+if half_split != jaemin_paid:
+    if jaemin_paid > insoo_paid:
+        print(f"So 인수 owes 재민 ${round_num(half_split - insoo_paid)}")
+    else:
+        print(f"So 재민 owes 인수 ${round_num(half_split - jaemin_paid)}")
+
+"""
+jaemin_personal = round_num(total_jaemin_accounts - half_split)
+insoo_personal = round_num(total_insoo_accounts - half_split)
+
+print(f"재민 personal expenses: ${jaemin_personal}")
+print(f"인수 personal expenses: ${insoo_personal}")
+"""
+
+difference = round_num(total_spending - jaemin_paid - insoo_paid - total_sofi_accounts)
 if difference == 0:
     print("Everything added up!")
 else:
